e530cfece604f8bb16ac27103ec3420306bcd70f
len_cao <leon_cao@kingdee.com>
2019-05-22 12:06:27 +0800

    头文件修改

---
+++ YZJMessageSDK.framework/Headers/KDGlobleSearchFileViewController.h
@@ -0,0 +1,13 @@
+//
+//  KDGlobleSearchFileViewController.h
+//  kdweibo
+//
+//  Created by he15his on 2018/1/16.
+//  Copyright © 2018年 www.kingdee.com. All rights reserved.
+//
+
+#import "KDGlobleSearchAllViewController.h"
+
+@interface KDGlobleSearchFileViewController : KDGlobleSearchAllViewController
+
+@end
